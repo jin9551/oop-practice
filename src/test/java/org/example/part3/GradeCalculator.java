8c3f3b2c3fa9e6602e5781cdca8469cc06471d02
jin9551 <wlsgk9551@gmail.com>
2025-06-14 22:17:55 +0900

    2025.06.14

---
+++ src/test/java/org/example/part3/GradeCalculator.java
@@ -0,0 +1,11 @@
+package org.example.part3;
+
+/**
+ * 요구 사항
+ * 평균학점 계산 방법 = (학점수*교과목 평점)의 합계/수강신청 총 학점 수
+ * 일급 컬렉션 사용
+ */
+
+public class GradeCalculator {
+    // 학점계산기 도메인 : 이수한 과목들, 학점 계산기
+}
